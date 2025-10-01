224a8e3898758a239e6999fe4d8c01c3133a1532
Isabel <isabelleewy@gmail.com>
2025-10-01 23:55:00 +0800

    Change flat distribution to piechart

--- components/flat_distribution.py
+++ components/flat_distribution.py
@@ -2,10 +2,30 @@ import streamlit as st
 import plotly.express as px
 
 def show_flat_distribution(df):
-    with st.container(border=True):
+    with st.container(border=True, height="stretch"):
         flat_counts = df["flat_type"].value_counts().reset_index()
         flat_counts.columns = ["Flat Type", "Transactions"]
 
-        fig = px.bar(flat_counts, x="Flat Type", y="Transactions")
+        fig = px.pie(
+            flat_counts,
+            names="Flat Type",
+            values="Transactions",
+            hole=0.3,
+        )
+
+        fig.update_traces(
+            textinfo="percent",
+            pull=[0.04] * len(flat_counts),
+        )
+
+        fig.update_layout(
+            margin=dict(l=20, r=20, t=40, b=20),
+            legend=dict(
+                orientation="h",          # horizontal legend
+                yanchor="top", y=-0.1,    # push below chart
+                xanchor="center", x=0.5,  # center align
+            )
+        )
+
         st.markdown("## 🏠 Flat Type Distribution")
         st.plotly_chart(fig, use_container_width=True)
